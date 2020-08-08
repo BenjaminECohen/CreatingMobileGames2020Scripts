23a60cd5f1c680f759d3cf81b80986d564932f03
Benjamin Cohen <cohenb@ufl.edu>
2020-08-08 19:15:52 -0400

    8_8_2020_HOTFIX
    IDK how, but weird shit happened. Old stuff go pushed, new stuff didn't. This will fix previous push to actual current state.

---
+++ BaseScriptPackage/EnemyTurret.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTurret : MonoBehaviour
+{
+    public enum Behavior
+    {
+        [Tooltip("Fires a projectile only when the player is in the trigger area.")]
+        TriggerArea,
+        [Tooltip("Always fire projectiles.")]
+        ConstantFire
+    }
+
+    public Behavior behavior = Behavior.ConstantFire;
+
+    [Tooltip("In projectiles per second")]
+    public float fireRate = 2;
+
+    [Tooltip("The projectile that has the Weapon script on it.")]
+    public GameObject projectilePrefab;
+
+    [Tooltip("The spawn position of the projectile.")]
+    public Transform projectileSpawnPosition;
+    private Vector3 originalPositionOfSpawn;
+
+    private SpriteRenderer spriteRenderer;
+
+    private int directionalMultiplier = 1;
+
+
+    //projectile firing
+    private float lastShotTime = 0;
+
+    [Header("Sound")]
+    public bool useSoundEffect;
+    public AudioSource audioSource;
+    //public AudioClip attackSoundEffect;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        originalPositionOfSpawn = projectileSpawnPosition.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Handles direction
+        if (spriteRenderer.flipX)
+        {
+            directionalMultiplier = -1;
+            projectileSpawnPosition.localPosition = new Vector3(originalPositionOfSpawn.x * -1, originalPositionOfSpawn.y, originalPositionOfSpawn.z);
+        }
+        else
+        {
+            directionalMultiplier = 1;
+            projectileSpawnPosition.localPosition = originalPositionOfSpawn;
+        }
+
+        if (behavior == Behavior.ConstantFire)
+        {
+            TimeCheck();
+        }
+
+    }
+
+    private void FireProjectile()
+    {
+        GameObject projectile = Instantiate(this.projectilePrefab, projectileSpawnPosition.position, Quaternion.identity);
+        Weapon.Projectile projectileStats = projectile.GetComponent<Weapon>().projectile;
+        Rigidbody2D projectileRB = projectile.GetComponent<Rigidbody2D>();
+
+        Weapon wep = projectile.GetComponent<Weapon>();
+
+        projectileRB.gravityScale = projectileStats.projectileGravityScale;
+        projectileRB.AddForce(projectile.transform.right * directionalMultiplier * projectileStats.projectileSpeed);
+
+        if (useSoundEffect && wep.attackSoundEffect)
+        {
+            audioSource.PlayOneShot(wep.attackSoundEffect);
+        }
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (behavior == Behavior.TriggerArea)
+            {
+                TimeCheck();
+            }
+        }
+        
+    }
+
+
+    private void TimeCheck()
+    {
+        if (Time.time - lastShotTime >= (1 / fireRate))
+        {
+            FireProjectile();
+            lastShotTime = Time.time;
+        }
+    }
+
+
+}
---
+++ BaseScriptPackage/PlayerAttack_SideScroller_Durability.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Animator))]
+
+public class PlayerAttack_SideScroller_Durability : MonoBehaviour
+{
+
+    //This script handles the animation of attacking on a button press. 
+    
+    //It is also necessary to use for the weapon switching script and attack behavior script
+
+    private Animator animator;
+    public Weapon weapon;
+
+    private int durability;
+    public int MAX_DURABILITY = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        durability = MAX_DURABILITY;
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && !animator.GetBool("Attack"))
+        {
+            if (weapon && durability > 0)
+            {
+                //ATTACK!
+                durability--;
+                animator.SetBool("Attack", true);
+
+
+                if (durability <= 0)
+                {
+                    weapon = null;
+                    durability = MAX_DURABILITY;
+                }
+
+            }
+            
+        }
+    }
+}
